dd59e18a982d40c67352527f67520f8f5e84008a
phkundi <p.kundratitz@hotmail.com>
2024-10-29 18:13:17 +0100

    add loading state while auth not ready

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <NuxtPage />
+  <div
+    class="h-screen flex justify-center items-center"
+    v-if="!authStore.isInitialized"
+  >
+    <UtilityLoadingSpinner color="grey" />
+  </div>
+  <NuxtPage v-else />
 </template>
 <script setup>
 const authStore = useAuthStore();
